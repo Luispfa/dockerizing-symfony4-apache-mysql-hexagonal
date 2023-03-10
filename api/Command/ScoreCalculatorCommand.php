e503aeb90b3d9244db6954886a457ca58dcde277
Luis Flores <luispfa@gmail.com>
2023-03-10 21:33:17 +0100

    rename classes

--- api/Command/ScoreCalculatorCommand.php
+++ api/Command/ScoreCalculatorCommand.php
@@ -4,24 +4,28 @@ declare(strict_types=1);
 
 namespace Api\Command;
 
-use App\Application\CalculateScore;
+use App\Application\AdAllFinder;
+use App\Application\ScoreCalculator;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CalculateScoreCommand extends Command
+class ScoreCalculatorCommand extends Command
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:calculate-score';
     protected static $defaultDescription = 'Calcula score of Ads.';
 
-    private $calculate;
+    private $scoreCalculator;
+    private $adAllFinder;
 
-    public function __construct(CalculateScore $calculate)
+    public function __construct(ScoreCalculator $scoreCalculator, AdAllFinder $adAllFinder)
     {
-        $this->calculate = $calculate;
+        $this->scoreCalculator = $scoreCalculator;
+        $this->adAllFinder = $adAllFinder;
+
         parent::__construct();
     }
 
@@ -38,11 +42,13 @@ class CalculateScoreCommand extends Command
             throw new \LogicException('This command accepts only an instance of "ConsoleOutputInterface".');
         }
 
-        $calculate = $this->calculate->__invoke()->ads();
+        $this->scoreCalculator->__invoke();
+
+        $getAll = $this->adAllFinder->__invoke()->ads();
 
         $table = new Table($output);
         $table->setHeaders(['id', 'typology',  'houseSize', 'gardenSize', 'score']);
-        foreach ($calculate as ['id' => $id, 'typology' => $typology, 'houseSize' => $houseSize, 'gardenSize' => $gardenSize, 'score' => $score]) {
+        foreach ($getAll as ['id' => $id, 'typology' => $typology, 'houseSize' => $houseSize, 'gardenSize' => $gardenSize, 'score' => $score]) {
             $table->addRow([$id, $typology, $houseSize, $gardenSize, $score]);
         }
         $table->render();
