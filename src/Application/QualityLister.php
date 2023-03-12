79a20ec4f4f12da414e86ea735ea4abeed27363c
Luis Flores <luispfa@gmail.com>
2023-03-12 01:31:39 +0100

    QualityLister: persist data in a JSON file

--- src/Application/QualityLister.php
+++ src/Application/QualityLister.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Application;
 
+use App\Application\Bus\Command\ScoreCalculatorCommandHandler;
 use App\Application\Bus\Query\QualityListerQueryHandler;
 use App\Application\Response\AdResponse;
 use App\Application\Response\AdsResponse;
@@ -11,16 +12,21 @@ use function Lambdish\Phunctional\map as PhunctionalMap;
 
 final class QualityLister
 {
-    private $queryHandler;
+    private $qualityListerQueryHandler;
+    private $scoreCalculatorCommandHandler;
 
-    public function __construct(QualityListerQueryHandler $queryHandler)
-    {
-        $this->queryHandler = $queryHandler;
+    public function __construct(
+        QualityListerQueryHandler $qualityListerQueryHandler,
+        ScoreCalculatorCommandHandler $scoreCalculatorCommandHandler
+    ) {
+        $this->qualityListerQueryHandler = $qualityListerQueryHandler;
+        $this->scoreCalculatorCommandHandler = $scoreCalculatorCommandHandler;
     }
 
     public function __invoke(): AdsResponse
     {
-        $ad = $this->queryHandler->__invoke();
+        $this->scoreCalculatorCommandHandler->__invoke();
+        $ad = $this->qualityListerQueryHandler->__invoke();
 
         return new AdsResponse(...PhunctionalMap(AdResponse::toResponse(), $ad));
     }
